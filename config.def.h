d33d2f0a735e6aee2add572b2cbf2535b07400ac
j0giwa <jonasschwin20022@gmx.de>
2023-01-13 17:38:08 +0100

    switched to a i3-layout

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 15;       /* snap pixel */
 
 /* Vanitygaps */
 static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
@@ -19,16 +19,16 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=8" };
 static const char dmenufont[]       = "monospace:size=8";
 
 static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char normbordercolor[]       = "#333333";
+static char normfgcolor[]           = "#888888";
+static char selfgcolor[]            = "#FFFFFF";
+static char selbordercolor[]        = "#4C7899";
+static char selbgcolor[]            = "#285577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
