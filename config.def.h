40841e679e05cfceac597c96c28a73532bebdd12
j0giwa <jonasschwind20022@gmx.de>
2023-06-30 13:37:46 +0200

    Fixed transparency bug

--- config.def.h
+++ config.def.h
@@ -76,6 +76,10 @@ static char *colors[][3] = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "dev", "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx" };
+//static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+//static const char *tags[] = { "", "爵", "", "", "", "", "", "", "" };
+
 
 static const Rule rules[] = {
 	/* xprop(1):
