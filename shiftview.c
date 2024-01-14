d97992bf93433a588452407139a4b675c32ec64c
j0giwa <jonasschwind20022@gmx.de>
2024-01-14 15:32:58 +0100

    Test

--- shiftview.c
+++ shiftview.c
@@ -4,7 +4,8 @@
  *          or left (negative value)
  */
 void
-shiftview(const Arg *arg) {
+shiftview(const Arg *arg)
+{
 	Arg shifted;
 
 	if(arg->i > 0) // left circular shift
@@ -17,3 +18,36 @@ shiftview(const Arg *arg) {
 
 	view(&shifted);
 }
+
+void
+shifttag(const Arg *arg)
+{
+	Arg a;
+	Client *c;
+	unsigned visible = 0;
+	int i = arg->i;
+	int count = 0;
+	int nextseltags, curseltags = selmon->tagset[selmon->seltags];
+
+	do {
+		if(i > 0) // left circular shift
+			nextseltags = (curseltags << i) | (curseltags >> (LENGTH(tags) - i));
+
+		else // right circular shift
+			nextseltags = (curseltags >> - i) | (curseltags << (LENGTH(tags) + i));
+
+                // Check if tag is visible
+		for (c = selmon->clients; c && !visible; c = c->next)
+			if (nextseltags & c->tags) {
+				visible = 1;
+				break;
+			}
+		i += arg->i;
+	} while (!visible && ++count < 10);
+
+	if (count < 10) {
+		a.i = nextseltags;
+		tag(&a);
+	}
+}
+
