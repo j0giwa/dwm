47ea20e4acc1fa27ec2b563dc657b4e6e86474cc
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-04-23 15:13:25 +0200

    Update tag icons

--- config.def.h
+++ config.def.h
@@ -61,10 +61,11 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"dev",  "www", "sys", "doc", "vbox", "chat", "mus", "vid", "gfx"};
-static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = {"󰣇",  "󰖟", "󰖷", "󰈙", "󰢔", "󰭹", "󰝚", "󰕧", "󰏘"};
+static const char *tagsalt[] = { "󰎤", "󰎧", "󰎪", "󰎭", "󰎱", "󰎳", "󰎶", "󰎹", "󰎼" };
 static const int momentaryalttags = 0; /* 1 means alttags will show only when key is held down*/
- 
+
+/* window rules */ 
 static const Rule rules[] = {
     /* xprop(1):
      *	WM_CLASS(STRING) = instance, class
