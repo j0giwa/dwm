eb95bb214b6cf1fb7e0fc85c17100f0696484928
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-04-29 13:29:20 +0200

    Added tag labels

--- config.def.h
+++ config.def.h
@@ -63,6 +63,9 @@ static char *colors[][3] = {
 
 /* tagging */
 static const char *tags[] = {"", "", "󰒓", "󰈙", "", "󰭹", "󰝚", "󰕧", "󰏘"};
+static const char ptagf[] = "%s %s";	/* format of a tag label */
+static const char etagf[] = "%s";	/* format of an empty tag */
+static const int lcaselbl = 0;		/* 1 means make tag label lowercase */	
 
 /* window rules */ 
 static const Rule rules[] = {
